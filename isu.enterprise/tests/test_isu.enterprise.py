10dfaaae97c69f6140167f8944b6a6bf7ef352e2
Evgeny Cherkashin <eugene@irnok.net>
2017-02-13 20:47:42 +0800

    Added package definition.

---
+++ isu.enterprise/tests/test_isu.enterprise.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class isu.enterpriseTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        self.assertEqual(1 + 1, 2)
+
+    def tearDown(self):
+        pass
