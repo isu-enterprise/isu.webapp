ffe831e4d728ce8a28c57bedeeb39e3efad594f6
Evgeny Cherkashin <eugene@irnok.net>
2017-02-22 23:06:57 +0800

    Arranged attributes in a schema.

--- src/isu/enterprise/interfaces.py
+++ src/isu/enterprise/interfaces.py
@@ -1,21 +1,42 @@
 # encoding:utf-8
 from zope.interface import Interface, Attribute
+from zope.i18nmessageid import MessageFactory
 import zope.schema
 
 # Implementation - реализация
 # Provide - обеспечить, **обслуживать**, оснащать.
 
-def _N(x):
-    return x
+
+_ = MessageFactory("isu.enterprise")
+
 
 class IAccountingEntry(Interface):
-    cr=Attribute("Credit account")
-    dr=Attribute("Debit account")
-    amount=Attribute("Amount of money sent")
-    currency=Attribute("A currency id")
-    moment=Attribute("A DateTime moment of the entry")
+    cr = Attribute("Credit account")
+    dr = Attribute("Debit account")
+    amount = Attribute("Amount of money sent")
+    currency = Attribute("A currency id")
+    moment = Attribute("A DateTime moment of the entry")
+
 
 class IDocument(Interface):
+    number = zope.schema.TextLine(
+        title=_("Number"),
+        description=_("The number of the document,"
+                      "usually an unique member of some"
+                      "sequence."),
+        required=True,
+        # readonly=True,
+        # order=0
+    )
+
+    date = zope.schema.Datetime(
+        title=_("Дата"),
+        description=_("Дата проведения документа."),
+        readonly=True,
+        # order=1,
+        required=True
+    )
+
     def create():
         """
         Create document.
@@ -30,15 +51,16 @@ class IDocument(Interface):
         and create a set of accounting entries.
         """
 
+
 class ICreditSlip(IDocument):
     """Приходный ордер
     """
 
-    reason=zope.schema.TextLine(\
-        title=_N(u"Reason"),
-        description=_N(u"The reason of the peration to be conducted."),
-        required = True
-        )
+    reason = zope.schema.TextLine(
+        title=_(u"Reason"),
+        description=_(u"The reason of the peration to be conducted."),
+        required=True,
+    )
 
     def addentry(entry):
         """
@@ -46,11 +68,14 @@ class ICreditSlip(IDocument):
         entry in the credit slip.
         """
 
+
 class IStorage(Interface):
+
     def store(document):
         """
         Store the document in storage
         """
+
 
 class IStorable(Interface):
     """Interface for description
@@ -61,6 +86,7 @@ class IStorable(Interface):
         """Store data of adapted component
         into the storage
         """
+
 
 class IConfigurator(Interface):
     """Interface defines an object, which
