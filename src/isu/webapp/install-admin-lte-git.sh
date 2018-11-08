11d0e20f0df9a78a98fb0391239fa7a403ab2ccf
Evgeny Cherkashin <eugeneai@irnok.net>
2018-11-08 18:05:40 +0800

    AdminLTE install scritp now creates target directory itself.

--- src/isu/webapp/install-admin-lte-git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo To install AdminLTE we need nasm package
-# FIXME: This is VERY stupid.
-mkdir -p node_modules
-git clone https://github.com/almasaeed2010/AdminLTE.git node_modules/admin-lte
-cd node_modules/admin-lte
-npm install --save 
--- src/isu/webapp/install-admin-lte.sh
+++ src/isu/webapp/install-admin-lte.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 echo To install AdminLTE we need nasm package
+rm -rf package*.json admin-lte node_modules
 npm install --save admin-lte
-cd node_modules/admin-lte
-npm install --save
+#cd node_modules/admin-lte
+#npm install --save
+set -e
+#pushd node_modules
+#shopt -s extglob
+#shopt -s dotglob
+#echo rm -rf ./!(admin-lte)
+mv node_modules/admin-lte ./
+rm -rf node_modules
+rm -rf admin-lte/build
+#popd
